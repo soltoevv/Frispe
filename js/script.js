79f04b831d8e911f5a7267b52e4f4c0ece69651f
bakay <csgo52833@gmail.com>
2022-05-31 19:21:50 +0300

    initial

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
+// const swiper = new Swiper(".mySwiper", {
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         renderBullet: function (index, className) {
+//             return '<span class="' + className + '">' + (index + 1) + "</span>";
+//         },
+//     },
+// });
+
+// const swiper = new Swiper(".mySwiper", {
+//     scrollbar: {
+//         el: ".swiper-scrollbar",
+//         hide: true,
+//     },
+// });
+
 const swiper = new Swiper(".mySwiper", {
-    spaceBetween: 50,
+    slidesPerView: 4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
 });
-var swiper2 = new Swiper(".mySwiper2", {
-    direction: "vertical",
-    spaceBetween: 50,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
+
+let btn = document.querySelector(".header-general__btn")
+btn.addEventListener("click", () => {
+    document.querySelector(".header-general__mobal").style.display = "block"
+})
+let btnclose = document.querySelector(".header-general__mobal__block__square h1")
+btnclose.addEventListener("click", () => {
+    document.querySelector(".header-general__mobal").style.display = "none"
+})
